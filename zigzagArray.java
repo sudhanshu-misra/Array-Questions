6212355b5ec9acb7f4bf0b0973bfc819c5d715b2
Sudhanshu Misra <103576112+sudhanshu-misra@users.noreply.github.com>
2023-06-25 17:39:03 +0530

    Add files via upload

---
+++ zigzagArray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class zigzagArray {
+    static int arr[] = new int[] { 4, 3, 7, 8, 6, 2, 1 };
+    static void zigZag()
+    {
+        Arrays.sort(arr);
+        
+        for (int i = 1; i <= arr.length - 2; i += 2) {
+           
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+
+    
+    public static void main(String[] args)
+    {
+        zigZag();
+        System.out.println(Arrays.toString(arr));
+    }
+}
